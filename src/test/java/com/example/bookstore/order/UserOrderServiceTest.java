568d5799477fba403cb64301e088f0c3dcab175b
END_K <end_karn@hotmail.com>
2020-12-14 16:59:35 +0700

    *Adjusted scope about calculate totalprice in kata potter logic*

--- src/test/java/com/example/bookstore/order/UserOrderServiceTest.java
+++ src/test/java/com/example/bookstore/order/UserOrderServiceTest.java
@@ -7,7 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class UserOrderServiceTest {
@@ -18,20 +19,20 @@ class UserOrderServiceTest {
     @Test
     void orderBook() {
         List<BookOrder> bookOrderList = new ArrayList<>();
-        BookOrder bookOrder1 = new BookOrder(1,1);
+        BookOrder bookOrder1 = new BookOrder((long) 1,1);
         bookOrderList.add(bookOrder1);
-        BookOrder bookOrder2 = new BookOrder(2,1);
+        BookOrder bookOrder2 = new BookOrder((long) 2,1);
         bookOrderList.add(bookOrder2);
-        BookOrder bookOrder3 = new BookOrder(3,1);
+        BookOrder bookOrder3 = new BookOrder((long) 3,1);
         bookOrderList.add(bookOrder3);
-        BookOrder bookOrder4 = new BookOrder(4,1);
+        BookOrder bookOrder4 = new BookOrder((long) 4,1);
         bookOrderList.add(bookOrder4);
-        BookOrder bookOrder5 = new BookOrder(5,1);
+        BookOrder bookOrder5 = new BookOrder((long) 5,1);
         bookOrderList.add(bookOrder5);
 
         OrderDetail actualResult = orderService.orderBook(bookOrderList);
 
-        assertEquals(375,actualResult.totalPrice);
+        assertEquals(500,actualResult.totalPrice);
         assertNotNull(actualResult.orderId);
     }
 }
